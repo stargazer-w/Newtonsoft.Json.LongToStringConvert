8f3b89c5e216a17cd109aa72c8303be3d6f095a5
沈伟 <330589465@qq.com>
2021-05-12 16:53:35 +0800

    添加项目文件。

---
+++ Stargazer.Extensions.Newtonsoft.Json.LongToStringConvert.Tests/LongToStringConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using Stargazer.Extensions.Newtonsoft.Json.LongToStringConvert;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Stargazer.Extensions.Newtonsoft.Json.LongToStringConvert
+{
+    [TestFixture()]
+    public class LongToStringConverterTests
+    {
+        [Test()]
+        public void ReadJsonTest()
+        {
+            var longNumber = 123456789L;
+            Assert.AreEqual("\"123456789\"", JsonConvert.SerializeObject(longNumber, new JsonSerializerSettings
+            {
+                Converters = { new LongToStringConverter() }
+            }));
+        }
+
+        [Test()]
+        public void WriteJsonTest()
+        {
+            var longNumber = "123456789";
+            Assert.AreEqual(123456789L, JsonConvert.DeserializeObject<long>(longNumber, new JsonSerializerSettings
+            {
+                Converters = { new LongToStringConverter() }
+            }));
+            var longNumberString = "\"123456789\"";
+            Assert.AreEqual(123456789L, JsonConvert.DeserializeObject<long>(longNumberString, new JsonSerializerSettings
+            {
+                Converters = { new LongToStringConverter() }
+            }));
+        }
+    }
+}
---
+++ Stargazer.Extensions.Newtonsoft.Json.LongToStringConvert/LongToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Stargazer.Extensions.Newtonsoft.Json.LongToStringConvert
+{
+    public class LongToStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(long);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return JToken.ReadFrom(reader).Value<long>();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString());
+        }
+    }
+}
